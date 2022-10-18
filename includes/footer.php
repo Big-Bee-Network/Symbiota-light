6b646b0dd5691c9273dbf69e067506c0c1ca47c9
seltmann <enicospilus@gmail.com>
2022-10-18 14:46:44 -0700

    new menu

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 	<tr>
 		<td id="footer" colspan="3">
 			<p>
-				<a href="https://www.ccber.ucsb.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/UC_Santa_Barbara_Wordmark_Navy_RGB.png" style="width:250px" /></a>
+				<a href="https://www.ccber.ucsb.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CheadleCenterlogo.png" style="width:250px" /></a>
 				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:80px" /></a>
 				<a href="https://15.200.118.171/grants/awarded/ma-30-16-0387-16-0" target="_blank" title="IMLS"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/imls-logo_0.png" style="width:200px" /></a>
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:210px" /></a>
