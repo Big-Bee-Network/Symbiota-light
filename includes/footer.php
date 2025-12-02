c80d4bc1ed40b97e09ddcb0bc461fcb1552e57a3
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-12-02 15:17:01 -0800

    Update footer file

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,37 @@
 <footer>
 	<div class="logo-gallery">
-				<a href="https://www.ccber.ucsb.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CheadleCenterlogo.png" style="width:250px" /></a>
-				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:80px" /></a>
-				<a href="http://idigbio.org" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:160px" /></a>
-				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:210px" /></a>
+		<?php
+		//include($SERVER_ROOT . '/accessibility/module.php');
+		?>
+		<a href="https://www.ccber.ucsb.edu" target="_blank">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/CheadleCenterlogo.png" />
+		</a>
+		<a href="https://www.nsf.gov" target="_blank" title="<?= $LANG['F_NSF'] ?>" aria-label="<?= $LANG['F_VISIT_NSF'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="<?= $LANG['F_NSF_LOGO'] ?>" />
+		</a>
+		<a href="http://idigbio.org" target="_blank" title="iDigBio" aria-label="<?= $LANG['F_VISIT_IDIGBIO'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="<?= $LANG['F_IDIGBIO_LOGO'] ?>" />
+		</a>
+		<!-- <a href="https://biodiversity.ku.edu" target="_blank" title="<?= $LANG['F_KU-BI'] ?>" aria-label="<?= $LANG['F_KU-BI'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/ku-bi_logo.png"  alt="<?= $LANG['F_KU-BI_LOGO'] ?>" />
+		</a> -->
+		<a href="https://symbiota.org/" target="_blank" title="<?= $LANG['F_SSH'] ?>" aria-label="<?= $LANG['F_SSH'] ?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/SSH.png"  alt="<?= $LANG['F_SSH_LOGO'] ?>" />
+		</a>
 	</div>
-	<div style="text-align:center;margin-bottom:15px;margin-left:20px">This project made possible by National Science Foundation Awards:
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2102006" target="_blank">2102006</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101929" target="_blank">2101929</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101908" target="_blank">2101908</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101876" target="_blank">2101876</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101875" target="_blank">2101875</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101851" target="_blank">2101851</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101345" target="_blank">2101345</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101913" target="_blank">2101913</a>,
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101891" target="_blank">2101891</a> and
-		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101850" target="_blank">2101850</a>.
 	<p>
-                <?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
-        </p>
-	</div>
+		<?= $LANG['F_NSF_AWARDS'] ?> <a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2102006" target="_blank">#2102006</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101929" target="_blank">2101929</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101908" target="_blank">2101908</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101876" target="_blank">2101876</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101875" target="_blank">2101875</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101851" target="_blank">2101851</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101345" target="_blank">2101345</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101913" target="_blank">2101913</a>, 
+		<a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101891" target="_blank">2101891</a>, 
+		and <a href="https://www.nsf.gov/awardsearch/show-award/?AWD_ID=2101850" target="_blank">2101850</a>.
+	</p>
+	<p>
+		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
+	</p>
 </footer>
